3102083d076cbf7fb95f11939fd89cc18660276e
sejarce <sejarce@sina.com>
2016-12-06 15:58:35 +0800

    Add files via upload

---
+++ Export.h
@@ -0,0 +1,16 @@
+#ifndef _EXPORT_H
+#define _EXPORT_H
+
+#ifdef ENGINE_EXPORTS
+
+#ifndef ENGINE_DLLCLASS_EXPORTS
+#define ENGINE_DLLCLASS_EXPORTS __declspec(dllexport) 
+#endif
+#else
+#ifndef ENGINE_DLLCLASS_EXPORTS
+#define ENGINE_DLLCLASS_EXPORTS __declspec(dllimport) 
+#endif
+
+#endif
+
+#endif
